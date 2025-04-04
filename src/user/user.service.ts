737ecf40a6fb67d5aff4c036f8f7bc2c1b18379e
armcortex3445 <wlghks5966@naver.com>
2025-04-04 17:26:55 +0900

    feat(UserModule) : add user create logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { QueryRunner, Repository } from 'typeorm';
 import { ServiceException } from '../_common/filter/exception/service/service-exception';
 import { createTransactionQueryBuilder } from '../db/query-runner/query-Runner.lib';
+import { CreateUserDTO } from './dto/create-user.dto';
 import { User } from './entity/user.entity';
 
 @Injectable()
@@ -54,6 +55,28 @@ export class UserService extends TypeOrmCrudService<User> {
         'EXTERNAL_SERVICE_FAILED',
         'INTERNAL_SERVER_ERROR',
         `Can't update username`,
+        { cause: error },
+      );
+    }
+  }
+
+  async createUser(queryRunner: QueryRunner, dto: CreateUserDTO): Promise<User> {
+    try {
+      const result = await createTransactionQueryBuilder(queryRunner, User)
+        .insert()
+        .into(User)
+        .values([
+          {
+            ...dto,
+          },
+        ])
+        .execute();
+      return result.generatedMaps[0] as User;
+    } catch (error) {
+      throw new ServiceException(
+        'EXTERNAL_SERVICE_FAILED',
+        'INTERNAL_SERVER_ERROR',
+        `Can't create User`,
         { cause: error },
       );
     }
