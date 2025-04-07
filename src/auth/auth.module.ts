0a1d148be82a2dcbf96bcb70e11840dd1239a39e
armcortex3445 <wlghks5966@naver.com>
2025-04-07 11:00:45 +0900

    refactor(VerificationModule) : move Repository logic
    - provide verification Repository logic from AuthModule
    - improve integrity of authentication logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,17 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { Verification } from './entity/verification.entity';
 import { BasicTokenGuard } from './guard/basic-auth.guard';
 import { OwnerGuard } from './guard/owner.guard';
 import { RolesGuard } from './guard/role.guard';
 import { TokenAuthGuard } from './guard/token-auth.guard';
 
 @Module({
-  imports: [JwtModule, forwardRef(() => UserModule)],
+  imports: [TypeOrmModule.forFeature([Verification]), JwtModule, forwardRef(() => UserModule)],
   controllers: [AuthController],
   providers: [AuthService, BasicTokenGuard, TokenAuthGuard, OwnerGuard, RolesGuard],
   exports: [AuthService, OwnerGuard, TokenAuthGuard, BasicTokenGuard, RolesGuard],
