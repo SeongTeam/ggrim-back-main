c2fbf066dea73d58c64aaadeb11c32c5de845161
armcortex3445 <wlghks5966@naver.com>
2025-04-09 13:23:19 +0900

    doc(AuthModule) : add TODO

--- src/auth/entity/verification.entity.ts
+++ src/auth/entity/verification.entity.ts
@@ -2,6 +2,8 @@ import { IsDate, IsEmail, IsString, IsUUID } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import { CustomBaseEntity } from '../../db/entity/custom.base.entity';
 
+// TODO 인증 로직 보안 강화
+// [ ] : 시도 횟수 정보 저장하여 무차별 대입 예방하기
 @Entity()
 @Unique(['email'])
 export class Verification extends CustomBaseEntity {
