7eb4215de80e9e2b11ca60c356789c08cfacfbee
armcortex3445 <wlghks5966@naver.com>
2025-04-10 16:39:53 +0900

    refactor(AuthModule ) : apply renamed Interface

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,7 +23,7 @@ import { createTransactionQueryBuilder } from '../db/query-runner/query-Runner.l
 import { User, UserRole } from '../user/entity/user.entity';
 import { OneTimeToken, OneTimeTokenPurpose } from './entity/one-time-token.entity';
 import { Verification } from './entity/verification.entity';
-import { OneTimeTokenGuardResult } from './guard/one-time-token.guard';
+import { OneTimeTokenPayload } from './guard/type/request-payload';
 
 export type TokenType = 'REFRESH' | 'ACCESS' | 'ONE_TIME';
 export type StandardTokenPurpose = 'access' | 'refresh';
@@ -297,7 +297,7 @@ export class AuthService {
     return oneTimeToken;
   }
 
-  async markOneTimeJWT(queryRunner: QueryRunner, info: OneTimeTokenGuardResult) {
+  async markOneTimeJWT(queryRunner: QueryRunner, info: OneTimeTokenPayload) {
     const { oneTimeTokenID, oneTimeToken } = info;
     await this.updateOneTimeToken(queryRunner, oneTimeTokenID, { used_date: new Date() });
   }
