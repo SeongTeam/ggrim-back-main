d24fae778ea47a8b39459d74285cb6480abc8a57
armcortex3445 <wlghks5966@naver.com>
2025-04-11 12:08:44 +0900

    doc(AuthModule) : update TODO

--- src/auth/entity/verification.entity.ts
+++ src/auth/entity/verification.entity.ts
@@ -3,7 +3,8 @@ import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import { CustomBaseEntity } from '../../db/entity/custom.base.entity';
 
 // TODO 인증 로직 보안 강화
-// [ ] : 시도 횟수 정보 저장하여 무차별 대입 예방하기
+// [ ] : 무차별 대입 예방 로직 구현(시도 횟수 정보 저장, IP 주소 Rate Liming, Captcha 적용 등)
+//  => 현재는 만료 기간이 5분, 짧으므로 당장은 불필요.
 @Entity()
 @Unique(['email'])
 export class Verification extends CustomBaseEntity {
