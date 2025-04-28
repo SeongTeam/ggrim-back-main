0bb16fe1edfa5d69699b5f624a0fbfa400ef6250
armcortex3445 <wlghks5966@naver.com>
2025-04-28 16:02:13 +0900

    doc(QuizModule) : add TODO

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -361,6 +361,7 @@ export class QuizService extends TypeOrmCrudService<Quiz> {
   // [ ] : 앱 종료 훅 시점에, 플러시 실패에 대응 로직 추가하기
   // [ ] : 시스템 확장시, CONNECTION_POOL_SIZE 증가도 고려하기
   //  -> : MAX CONNECTION POOL 등의 앱 메모리 및 리소스 관리 고려할 것
+  // [ ] : Quiz Flush 로직 통합 또는 Redis 사용 고려하기
 
   async flushViewMap() {
     const key: keyof Quiz = 'view_count';
