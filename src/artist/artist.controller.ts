dcaa9b92b42d127c621d6df61f5dd2f3c403a245
armcortex3445 <wlghks5966@naver.com>
2025-04-29 13:21:31 +0900

    feat(ArtistModule) : add Authorization
    - prevent user to access API

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,5 +1,15 @@
-import { Crud, CrudController } from '@dataui/crud';
-import { Controller } from '@nestjs/common';
+import {
+  Crud,
+  CrudController,
+  CrudRequest,
+  Override,
+  ParsedBody,
+  ParsedRequest,
+} from '@dataui/crud';
+import { Controller, UseGuards } from '@nestjs/common';
+import { TokenAuthGuard } from '../auth/guard/authentication/token-auth.guard';
+import { RolesGuard } from '../auth/guard/authorization/roles.guard';
+import { Roles } from '../user/decorator/role';
 import { ArtistService } from './artist.service';
 import { CreateArtistDTO } from './dto/create-artist.dto';
 import { Artist } from './entities/artist.entity';
@@ -39,4 +49,22 @@ const EXCLUDED_COLUMN = ['created_date', 'updated_date', 'deleted_date', 'versio
 @Controller('artist')
 export class ArtistController implements CrudController<Artist> {
   constructor(public service: ArtistService) {}
+
+  @Override('createOneBase')
+  @UseGuards(TokenAuthGuard, RolesGuard)
+  async createOne(@ParsedRequest() req: CrudRequest, @ParsedBody() dto: CreateArtistDTO) {
+    return this.service.createOne(req, dto);
+  }
+
+  @Override('replaceOneBase')
+  @UseGuards(TokenAuthGuard, RolesGuard)
+  async replaceOne(@ParsedRequest() req: CrudRequest, @ParsedBody() dto: CreateArtistDTO) {
+    return this.service.replaceOne(req, dto);
+  }
+  @Override('deleteOneBase')
+  @Roles('admin')
+  @UseGuards(TokenAuthGuard, RolesGuard)
+  async deleteOne(@ParsedRequest() req: CrudRequest) {
+    return this.service.deleteOne(req);
+  }
 }
--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ArtistService } from './artist.service';
-import { ArtistController } from './artist.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
+import { ArtistController } from './artist.controller';
+import { ArtistService } from './artist.service';
 import { Artist } from './entities/artist.entity';
 import { wikiArtArtist } from './entities/wikiArt-artist.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Artist, wikiArtArtist])],
+  imports: [TypeOrmModule.forFeature([Artist, wikiArtArtist]), AuthModule, UserModule],
   controllers: [ArtistController],
   providers: [ArtistService],
   exports: [ArtistService],
