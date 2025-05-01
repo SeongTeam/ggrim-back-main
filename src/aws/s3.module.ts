f744bac9c721352b4e7868a748ea248041b80e24
armcortex3445 <wlghks5966@naver.com>
2025-05-01 13:42:07 +0900

    feat(S3Module) : add Auth logic

--- src/aws/s3.module.ts
+++ src/aws/s3.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 import { S3Controller } from './s3.controller';
 import { S3Service } from './s3.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule],
   controllers: [S3Controller],
   providers: [S3Service],
   exports: [S3Service],
