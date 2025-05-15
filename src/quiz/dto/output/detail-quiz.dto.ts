04e6832390f34c3e3e5dceeab26eb4673410623e
armcortex3445 <wlghks5966@naver.com>
2025-05-15 19:52:20 +0900

    feat(QuizModule) : update DetailQuizDTO
    - exclude sensitive user info

--- src/quiz/dto/output/detail-quiz.dto.ts
+++ src/quiz/dto/output/detail-quiz.dto.ts
@@ -1,9 +1,31 @@
+import { OmitType } from '@nestjs/mapped-types';
+import { plainToInstance } from 'class-transformer';
+import { ShortUser } from '../../../user/interface/short-user';
 import { Quiz } from '../../entities/quiz.entity';
 import { QuizReactionCount } from '../../interface/reaction-count';
 import { QuizReactionType } from '../quiz-reaction.dto';
 
 export class DetailQuizDTO {
-  quiz!: Quiz;
+  quiz!: ShowQuiz;
   reactionCount!: QuizReactionCount;
   userReaction?: QuizReactionType;
+
+  constructor(quiz: Quiz, reactionCount: QuizReactionCount, userReaction?: QuizReactionType) {
+    this.quiz = ShowQuiz.createShowQuiz(quiz);
+    this.reactionCount = reactionCount;
+    this.userReaction = userReaction;
+  }
+}
+
+export class ShowQuiz extends OmitType(Quiz, ['owner'] as const) {
+  shortOwner!: ShortUser;
+
+  static createShowQuiz = (quiz: Quiz): ShowQuiz => {
+    const { owner, ...rest } = quiz;
+
+    const showQuiz = plainToInstance(ShowQuiz, rest);
+    const shortOwner = new ShortUser(owner);
+    showQuiz.shortOwner = shortOwner;
+    return showQuiz;
+  };
 }
