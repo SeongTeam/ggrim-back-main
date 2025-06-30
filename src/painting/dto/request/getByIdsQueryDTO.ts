3b1d30b616ec9d7e40849ec5ae2f8006829a0d70
armcortex3445 <wlghks5966@naver.com>
2025-06-30 18:52:09 +0900

    refactor(painting): set eslint rule disable

--- src/painting/dto/request/getByIdsQueryDTO.ts
+++ src/painting/dto/request/getByIdsQueryDTO.ts
@@ -2,6 +2,7 @@ import { Transform } from "class-transformer";
 import { IsArray, IsUUID } from "class-validator";
 
 export class GetByIdsQueryDTO {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 	@Transform(({ value }) => (Array.isArray(value) ? value : [value]))
 	@IsArray()
 	@IsUUID("all", { each: true })
