71f5578f0a6bb2dc009499786e73242867d88733
armcortex3445 <wlghks5966@naver.com>
2025-07-01 13:37:53 +0900

    refactor(util): specify type

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,9 +1,10 @@
 import { ServiceException } from "../_common/filter/exception/service/serviceException";
 import { isNotFalsy } from "./validator";
 
-export function extractValues<T>(obj: T, key: keyof T): any[] {
-	const fieldValues = new Set<any>();
-
+export function extractValues<T, K extends keyof T, R = T[K] extends Array<infer U> ? U : T[K]>(
+	obj: T,
+	key: K,
+): R[] {
 	const field = obj[key];
 
 	if (!field) {
@@ -11,26 +12,29 @@ export function extractValues<T>(obj: T, key: keyof T): any[] {
 			"SERVICE_RUN_ERROR",
 			"INTERNAL_SERVER_ERROR",
 			`${String(key)} is falsy.
-      obj : ${JSON.stringify(obj, null, 2)}`,
+			obj : ${JSON.stringify(obj, null, 2)}`,
 		);
 	}
 
+	const fieldValues = new Set<R>();
 	if (Array.isArray(field)) {
-		field.forEach((value) => fieldValues.add(value));
-		return [...fieldValues];
+		field.forEach((value) => fieldValues.add(value as R));
+	} else {
+		fieldValues.add(field as R);
 	}
-
-	fieldValues.add(field);
-
 	return [...fieldValues];
 }
 
-export function extractValuesFromArray<T>(list: T[], key: keyof T): any[] {
-	const fieldValues = new Set<any>();
+export function extractValuesFromArray<
+	T,
+	K extends keyof T,
+	R = T[K] extends Array<infer U> ? U : T[K],
+>(list: T[], key: keyof T): R[] {
+	const fieldValues = new Set<R>();
 
 	list.forEach((obj) => {
 		const values = extractValues(obj, key);
-		values.forEach((value) => fieldValues.add(value));
+		values.forEach((value) => fieldValues.add(value as R));
 	});
 
 	return [...fieldValues];
