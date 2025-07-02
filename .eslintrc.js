c028c0b7e168d5f18b6c6d4055bf8ad50b98eca9
armcortex3445 <wlghks5966@naver.com>
2025-07-02 16:56:31 +0900

    chore(eslint): reformat configuration file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,21 @@ module.exports = {
 		tsconfigRootDir: __dirname,
 		sourceType: "module",
 	},
-	plugins: [
-		"@typescript-eslint", // TypeScript용 플러그인
-		"prettier", // Prettier와의 통합
-	],
-	extends: [
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking", // 타입 검사 필요한 규칙 추가
-		"plugin:prettier/recommended", // Prettier 통합 규칙
-	],
 	root: true,
 	env: {
 		node: true,
 		jest: true,
 		es6: true,
 	},
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking", // 타입 검사 필요한 규칙 추가
+		"plugin:prettier/recommended", // Prettier 통합 규칙
+		"prettier",
+	],
+	plugins: [
+		"@typescript-eslint", // TypeScript용 플러그인
+	],
 	ignorePatterns: [".eslintrc.js"],
 	rules: {
 		"@typescript-eslint/interface-name-prefix": "off",
