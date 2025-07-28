71554cb22a275202c14899dde967cbdbdf5ecca4
armcortex3445 <wlghks5966@naver.com>
2025-07-28 18:31:40 +0900

    feat(quiz): improve dto
    allow single data as input
    
    add IsOptional Validator

--- src/modules/quiz/dto/request/SearchQuiz.dto.ts
+++ src/modules/quiz/dto/request/SearchQuiz.dto.ts
@@ -1,6 +1,11 @@
 import { IsArray, IsString } from "class-validator";
+import { IsOptionalProperty } from "../../../_common/decorator/swagger/class-validator/isOptionalProperty";
+import { Transform } from "class-transformer";
 
 export class SearchQuizDTO {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+	@Transform(({ value }) => (Array.isArray(value) ? value : [value]))
+	@IsOptionalProperty()
 	@IsArray()
 	@IsString({ each: true })
 	artists: string[] = [];
@@ -11,6 +16,9 @@ export class SearchQuizDTO {
         - 서버쪽에서 파싱 로직을 사용해야함
       */
 
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+	@Transform(({ value }) => (Array.isArray(value) ? value : [value]))
+	@IsOptionalProperty()
 	@IsArray()
 	@IsString({ each: true })
 	tags: string[] = [];
@@ -20,6 +28,9 @@ export class SearchQuizDTO {
         - 예시) url?tags=["1","2"]
         - 서버쪽에서 파싱 로직을 사용해야함
       */
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+	@Transform(({ value }) => (Array.isArray(value) ? value : [value]))
+	@IsOptionalProperty()
 	@IsArray()
 	@IsString({ each: true })
 	styles: string[] = [];
