af8ed400bc42c43225400d3721c338b04df39d60
armcortex3445 <wlghks5966@naver.com>
2025-07-28 16:01:26 +0900

    feat(painting): delete private info

--- src/modules/painting/dto/response/shortPainting.response.ts
+++ src/modules/painting/dto/response/shortPainting.response.ts
@@ -6,15 +6,13 @@ export class ShortPaintingResponse {
 	readonly image_url: string;
 	readonly width: number;
 	readonly height: number;
-	readonly image_s3_key: string;
 
 	public constructor(painting: Painting) {
-		const { id, title, width, height, image_url, image_s3_key } = painting;
+		const { id, title, width, height, image_url } = painting;
 		this.id = id;
 		this.title = title;
 		this.width = width;
 		this.height = height;
 		this.image_url = image_url;
-		this.image_s3_key = image_s3_key;
 	}
 }
