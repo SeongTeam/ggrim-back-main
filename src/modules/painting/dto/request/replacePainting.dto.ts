13ddee79c2fb1fa2f9fda9a5adbae455ccbd8919
armcortex3445 <wlghks5966@naver.com>
2025-07-28 18:43:14 +0900

    feat(painting): revert dto

--- src/modules/painting/dto/request/replacePainting.dto.ts
+++ src/modules/painting/dto/request/replacePainting.dto.ts
@@ -1,7 +1,6 @@
 import { PickType } from "@nestjs/swagger";
 import { IsArray, IsString } from "class-validator";
 import { CreatePaintingDTO } from "./createPainting.dto";
-import { Transform } from "class-transformer";
 
 export class ReplacePaintingDTO extends PickType(CreatePaintingDTO, [
 	"title",
@@ -13,14 +12,10 @@ export class ReplacePaintingDTO extends PickType(CreatePaintingDTO, [
 	"artistName",
 	"image_s3_key",
 ]) {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-	@Transform(({ value }) => (Array.isArray(value) ? value : [value]))
 	@IsArray()
 	@IsString({ each: true })
 	tags!: string[];
 
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-	@Transform(({ value }) => (Array.isArray(value) ? value : [value]))
 	@IsArray()
 	@IsString({ each: true })
 	styles!: string[];
