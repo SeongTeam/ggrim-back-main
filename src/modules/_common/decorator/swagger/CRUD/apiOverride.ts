e52e96df45b50c87887000bac25b71ef3e574441
armcortex3445 <wlghks5966@naver.com>
2025-08-29 20:59:10 +0900

    feat(_common): improve @ApiOverride

--- src/modules/_common/decorator/swagger/CRUD/apiOverride.ts
+++ src/modules/_common/decorator/swagger/CRUD/apiOverride.ts
@@ -1,21 +1,33 @@
 import { BaseRouteName, Override } from "@dataui/crud";
-import { applyDecorators, Type } from "@nestjs/common";
+import { applyDecorators, HttpCode, HttpStatus, Type } from "@nestjs/common";
 import { ApiPaginationResponse } from "../apiPaginationResponse";
-import { ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
+import { ApiCreatedResponse, ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
 
 export function ApiOverride<TClass extends Type<any>>(
 	overrideKey: BaseRouteName,
 	responseDTO: TClass,
 ): MethodDecorator {
-	const prefix = "ManyBase";
+	const swaggers = [];
 
-	const swaggers = overrideKey.includes(prefix)
-		? [ApiPaginationResponse(responseDTO)]
-		: [
-				ApiOkResponse({
-					schema: { $ref: getSchemaPath(responseDTO) },
-				}),
-			];
+	if (overrideKey.includes("ManyBase")) {
+		swaggers.push(ApiPaginationResponse(responseDTO));
+	}
+
+	if (overrideKey.includes("create")) {
+		swaggers.push(
+			ApiCreatedResponse({
+				schema: { $ref: getSchemaPath(responseDTO) },
+			}),
+			HttpCode(HttpStatus.CREATED),
+		);
+	} else {
+		swaggers.push(
+			ApiOkResponse({
+				schema: { $ref: getSchemaPath(responseDTO) },
+			}),
+			HttpCode(HttpStatus.OK),
+		);
+	}
 
 	return applyDecorators(Override(overrideKey), ...swaggers);
 }
