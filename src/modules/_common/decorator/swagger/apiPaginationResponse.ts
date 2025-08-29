4c50b3e7741f1707f2c123cd2edcc2b56132aa9e
armcortex3445 <wlghks5966@naver.com>
2025-08-29 20:58:13 +0900

    feat(_common): add option into @ApiPaginationResponse

--- src/modules/_common/decorator/swagger/apiPaginationResponse.ts
+++ src/modules/_common/decorator/swagger/apiPaginationResponse.ts
@@ -1,40 +1,45 @@
-import { applyDecorators, Type } from "@nestjs/common";
-import { ApiDefaultResponse, ApiExtraModels, ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
+import { applyDecorators, HttpStatus, Type } from "@nestjs/common";
+import {
+	ApiCreatedResponse,
+	ApiDefaultResponse,
+	ApiExtraModels,
+	ApiOkResponse,
+	getSchemaPath,
+} from "@nestjs/swagger";
 import { Pagination } from "../../types";
 
-export const ApiPaginationResponse = <TClass extends Type<any>>(model: TClass) =>
-	applyDecorators(
-		ApiExtraModels(Pagination, model),
-		ApiOkResponse({
-			schema: {
-				title: `PaginatedResponseOf${model.name}`,
-				allOf: [
-					{ $ref: getSchemaPath(Pagination) },
-					{
-						properties: {
-							data: {
-								type: "array",
-								items: { $ref: getSchemaPath(model) },
-							},
-						},
+export const ApiPaginationResponse = <TClass extends Type<any>>(
+	model: TClass,
+	status: HttpStatus.OK | HttpStatus.CREATED = HttpStatus.OK,
+) => {
+	const schema = {
+		title: `PaginatedResponseOf${model.name}`,
+		allOf: [
+			{ $ref: getSchemaPath(Pagination) },
+			{
+				properties: {
+					data: {
+						type: "array",
+						items: { $ref: getSchemaPath(model) },
 					},
-				],
+				},
 			},
+		],
+	};
+	const responseSchemaMap = {
+		[HttpStatus.OK]: ApiOkResponse({
+			schema,
 		}),
+		[HttpStatus.CREATED]: ApiCreatedResponse({
+			schema,
+		}),
+	};
+
+	return applyDecorators(
+		ApiExtraModels(Pagination, model),
+		responseSchemaMap[status],
 		ApiDefaultResponse({
-			schema: {
-				title: `PaginatedResponseOf${model.name}`,
-				allOf: [
-					{ $ref: getSchemaPath(Pagination) },
-					{
-						properties: {
-							data: {
-								type: "array",
-								items: { $ref: getSchemaPath(model) },
-							},
-						},
-					},
-				],
-			},
+			schema,
 		}),
 	);
+};
