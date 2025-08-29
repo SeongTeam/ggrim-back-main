9cad8ab3662d3543ede38a9a3dd82d2cd0cc75d3
armcortex3445 <wlghks5966@naver.com>
2025-08-29 13:54:34 +0900

    refactor(auth): rename hash value field of  DTO

--- src/modules/auth/dto/response/showVerfication.response.ts
+++ src/modules/auth/dto/response/showVerfication.response.ts
@@ -4,14 +4,14 @@ export class ShowVerificationResponse {
 	readonly id: string;
 	readonly verification_success_date: Date | null;
 	readonly last_verified_date: Date | null;
-	readonly pin_code: string;
+	readonly hashedPinCode: string;
 	readonly pin_code_expired_date: Date;
 
 	constructor(verification: Verification) {
 		this.id = verification.id;
 		this.verification_success_date = verification.verification_success_date;
 		this.last_verified_date = verification.last_verified_date;
-		this.pin_code = verification.pin_code;
+		this.hashedPinCode = verification.pin_code;
 		this.pin_code_expired_date = verification.pin_code_expired_date;
 	}
 }
