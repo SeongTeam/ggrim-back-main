00e6ba2b36f9e9aa8a83dc17e6901275abd42581
armcortex3445 <wlghks5966@naver.com>
2025-08-30 18:42:14 +0900

    feat(test): increase timeout when debug with jest

--- test/_shared/test.service.ts
+++ test/_shared/test.service.ts
@@ -26,6 +26,9 @@ export class TestService {
 		if (process.env.NODE_ENV !== "test") {
 			throw new Error("ERROR-TEST-UTILS-ONLY-FOR-TESTS");
 		}
+		if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+			jest.setTimeout(60 * 1000 * 10); // 10 minutes
+		}
 	}
 
 	getAccessToken(user: User): string {
