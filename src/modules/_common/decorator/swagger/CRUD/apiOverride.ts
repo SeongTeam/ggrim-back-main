0d4676d02ed3d3aefb911d546e33aa99c0245107
armcortex3445 <wlghks5966@naver.com>
2025-08-30 13:23:12 +0900

    fix(_common): extend overrideKey

--- src/modules/_common/decorator/swagger/CRUD/apiOverride.ts
+++ src/modules/_common/decorator/swagger/CRUD/apiOverride.ts
@@ -3,27 +3,35 @@ import { applyDecorators, HttpCode, HttpStatus, Type } from "@nestjs/common";
 import { ApiPaginationResponse } from "../apiPaginationResponse";
 import { ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
 
-type UseRouteName = "getManyBase" | "createOneBase" | "replaceOneBase";
+type UseRouteName = "getManyBase" | "createOneBase" | "replaceOneBase" | "getOneBase";
 
 export function ApiOverride<TClass extends Type<any>>(
 	overrideKey: UseRouteName,
 	responseDTO: TClass,
 ): MethodDecorator {
 	// 각 상황별 Swagger 데코레이터 맵핑
-	const responseDecorators: Record<UseRouteName, MethodDecorator[]> = {
-		getManyBase: [ApiPaginationResponse(responseDTO, HttpStatus.OK), HttpCode(HttpStatus.OK)],
-		createOneBase: [
-			ApiOkResponse({
-				schema: { $ref: getSchemaPath(responseDTO) },
-			}),
-			HttpCode(HttpStatus.CREATED),
-		],
-		replaceOneBase: [
+
+	const DecoratorMaps = {
+		default: [
 			ApiOkResponse({
 				schema: { $ref: getSchemaPath(responseDTO) },
 			}),
 			HttpCode(HttpStatus.OK),
 		],
+		createOne: [
+			ApiOkResponse({
+				schema: { $ref: getSchemaPath(responseDTO) },
+			}),
+			HttpCode(HttpStatus.CREATED),
+		],
+
+		getMany: [ApiPaginationResponse(responseDTO, HttpStatus.OK), HttpCode(HttpStatus.OK)],
+	};
+	const responseDecorators: Record<UseRouteName, MethodDecorator[]> = {
+		getManyBase: DecoratorMaps.getMany,
+		createOneBase: DecoratorMaps.createOne,
+		replaceOneBase: DecoratorMaps.default,
+		getOneBase: DecoratorMaps.default,
 	};
 
 	return applyDecorators(Override(overrideKey), ...responseDecorators[overrideKey]);
