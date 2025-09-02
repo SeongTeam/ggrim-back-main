808a676f6eb09f62223a6c60f686a1c1185ddb63
armcortex3445 <wlghks5966@naver.com>
2025-09-02 16:42:56 +0900

    feat(utils): add object util logics

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -45,3 +45,25 @@ export function updateProperty<T>(obj: T, key: keyof T, value: T[keyof T] | unde
 		obj[key] = value;
 	}
 }
+
+export function pick<T extends object, K extends keyof T>(
+	obj: T,
+	selectedKeys: readonly K[],
+): Pick<T, K> {
+	const ret = Object.fromEntries(
+		Object.entries(obj).filter(([key]) => selectedKeys.includes(key as K)),
+	);
+
+	return ret as Pick<T, K>;
+}
+
+export function omit<T extends object, K extends keyof T>(
+	obj: T,
+	exclusiveKeys: readonly K[],
+): Omit<T, K> {
+	const ret = Object.fromEntries(
+		Object.entries(obj).filter(([key]) => !exclusiveKeys.includes(key as K)),
+	);
+
+	return ret as Omit<T, K>;
+}
