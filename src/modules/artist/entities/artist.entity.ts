d0aee5f46ade677d21d6d2c35f508d180c8dd158
armcortex3445 <wlghks5966@naver.com>
2025-09-03 18:09:33 +0900

    fix(artist): fix inaccurate entity type
    synchronize "date" type column between db and server
    at db, value is "yyyy-mm-dd" date.
    at server, value is Date object instance
    
    allow join field to be optional

--- src/modules/artist/entities/artist.entity.ts
+++ src/modules/artist/entities/artist.entity.ts
@@ -3,8 +3,8 @@ import { CustomBaseEntity } from "../../db/entity/customBase.entity";
 import { Painting } from "../../painting/entities/painting.entity";
 
 @Entity()
-/*TODO
-- 동명이인을 구분할 방법을 찾아야함
+/*TODO Artist 엔티티 개선
+- [ ] 동명이인 핸들링 방법 모색
 */
 @Unique(["name", "search_name"])
 export class Artist extends CustomBaseEntity {
@@ -23,12 +23,28 @@ export class Artist extends CustomBaseEntity {
 	@Column({
 		type: "date",
 		nullable: true,
+		transformer: {
+			from: (value: string | null): Date | null => {
+				return value ? new Date(value) : null;
+			},
+			to: (value: Date | null): string | null => {
+				return value ? value.toISOString().split("T")[0] : null;
+			},
+		},
 	})
 	birth_date!: Date | null;
 
 	@Column({
 		type: "date",
 		nullable: true,
+		transformer: {
+			from: (value: string | null): Date | null => {
+				return value ? new Date(value) : null;
+			},
+			to: (value: Date | null): string | null => {
+				return value ? value.toISOString().split("T")[0] : null;
+			},
+		},
 	})
 	death_date!: Date | null;
 
@@ -41,7 +57,7 @@ export class Artist extends CustomBaseEntity {
 	@OneToMany(() => Painting, (painting) => painting.artist, {
 		onDelete: "RESTRICT",
 	})
-	paintings!: Painting[];
+	paintings?: Painting[];
 
 	//combination UpperCase and '_'
 	@Column()
