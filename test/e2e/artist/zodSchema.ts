12256506818d93ca8e4424702b773b1c059ee513
armcortex3445 <wlghks5966@naver.com>
2025-09-03 18:12:11 +0900

    fix(e2e): fix Artist zodSchema

--- test/e2e/artist/zodSchema.ts
+++ test/e2e/artist/zodSchema.ts
@@ -6,12 +6,12 @@ export const zShowArtist = z.object({
 	id: z.string(),
 	name: z.string(),
 	image_url: z.string().nullable(),
-	birth_date: z.date().nullable(), // API 응답이 문자열이면 z.string().datetime().nullable()
-	death_date: z.date().nullable(), // 동일하게 z.string().datetime().nullable()
+	birth_date: z.string().nullable(), // API 응답이 문자열이면 z.string().datetime().nullable()
+	death_date: z.string().nullable(), // 동일하게 z.string().datetime().nullable()
 	info_url: z.string().nullable(),
 });
 
 // ShowArtistResponse 클래스에 대응되는 스키마
-export const ShowArtistResponse = zShowArtist.extend({
+export const zShowArtistResponse = zShowArtist.extend({
 	shortPaintings: z.array(zShowPainting),
 });
