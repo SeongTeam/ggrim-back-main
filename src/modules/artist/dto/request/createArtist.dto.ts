7c5cab5f03f293bca502bd7303c5b50cd652f595
armcortex3445 <wlghks5966@naver.com>
2025-10-14 12:10:48 +0900

    fix(artist): fix not transforming dto field

--- src/modules/artist/dto/request/createArtist.dto.ts
+++ src/modules/artist/dto/request/createArtist.dto.ts
@@ -1,15 +1,18 @@
 import { IsDate, IsString, IsUrl } from "class-validator";
 import { IsOptionalProperty } from "../../../_common/decorator/swagger/class-validator/isOptionalProperty";
+import { Type } from "class-transformer";
 
 export class CreateArtistDTO {
 	@IsString()
 	name!: string;
 
 	@IsOptionalProperty()
+	@Type(() => Date)
 	@IsDate()
 	birth_date?: Date;
 
 	@IsOptionalProperty()
+	@Type(() => Date)
 	@IsDate()
 	death_date?: Date;
 
