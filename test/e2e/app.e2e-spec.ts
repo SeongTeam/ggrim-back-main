f6345b404d962e6820cad2791107bddbab7bdd6b
armcortex3445 <wlghks5966@naver.com>
2025-11-13 15:12:42 +0900

    feat(test): add e2e test exam

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
-import * as request from "supertest";
+import { HttpStatus, INestApplication } from "@nestjs/common";
 import { AppModule } from "../../src/app.module";
-import { App } from "supertest/types";
+import createClient from "openapi-fetch";
+import { ApiPaths, paths } from "../openapi/dto-types";
 
 describe("AppController (e2e)", () => {
 	let app: INestApplication;
+	const port = 3001;
+	const client = createClient<paths>({ baseUrl: `http://localhost:${port}` });
 
 	beforeAll(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,13 +16,24 @@ describe("AppController (e2e)", () => {
 
 		app = moduleFixture.createNestApplication();
 		await app.init();
+		await app.listen(port);
 	});
 
-	it("/ (GET)", () => {
-		return request(app.getHttpServer() as App)
-			.get("/")
-			.expect(200)
-			.expect("Hello World!");
+	describe("/ (GET)", () => {
+		async function requestGetHello() {
+			const route = ApiPaths.AppController_getHello;
+			const response = await client.GET(route);
+
+			return response;
+		}
+
+		it("response should match the OpenAPI documentation. ", async () => {
+			const receivedRes = await requestGetHello();
+			const receivedStatus = receivedRes.response.status;
+			const receivedBody = receivedRes.data;
+			expect(receivedStatus).toBe(HttpStatus.OK);
+			expect(receivedBody).toEqual(["Hello World!"]);
+		});
 	});
 
 	afterAll(async () => {
